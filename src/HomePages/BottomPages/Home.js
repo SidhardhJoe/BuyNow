d55dcac5ae12908363ce34ef177e2722ca19f3b2
SidhardhJoe <sidhardhjoe@gmail.com>
2024-06-25 18:35:27 +0530

    1

--- src/HomePages/BottomPages/Home.js
+++ src/HomePages/BottomPages/Home.js
@@ -1,7 +1,10 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+
 
 const Home = () => {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <View style={styles.view1}>
