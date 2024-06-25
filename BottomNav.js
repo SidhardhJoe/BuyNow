5351175bcfdce2937b2f5c5a91fc82990409a230
SidhardhJoe <sidhardhjoe@gmail.com>
2024-06-25 16:00:23 +0530

    Bottom Nav Icons

--- BottomNav.js
+++ BottomNav.js
@@ -3,16 +3,53 @@ import Home from './src/HomePages/BottomPages/Home';
 import Cart from './src/HomePages/BottomPages/Cart';
 import Notifications from './src/HomePages/BottomPages/Notifications';
 import Profile from './src/HomePages/BottomPages/Profile';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const BottomNav = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown:false}}>
-        <Tab.Screen name='Home' component={Home}/>
-        <Tab.Screen name='Cart' component={Cart}/>
-        <Tab.Screen name='Notification' component={Notifications}/>
-        <Tab.Screen name='Profile' component={Profile}/>
+    <Tab.Navigator screenOptions={{ headerShown: false, }} >
+      <Tab.Screen name='Home' component={Home}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'home' : 'home-outline'}
+              size={size}
+              color={color}
+            />
+          )
+        }} />
+      <Tab.Screen name='Cart' component={Cart}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'cart' : 'cart-outline'}
+              size={size}
+              color={color}
+            />
+          )
+        }} />
+      <Tab.Screen name='Notification' component={Notifications}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'notifications' : 'notifications-outline'}
+              size={size}
+              color={color}
+            />
+          )
+        }} />
+      <Tab.Screen name='Profile' component={Profile}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <MaterialCommunityIcons
+              name={focused ? 'account' : 'account-outline'}
+              size={size}
+              color={color} />
+          )
+        }} />
     </Tab.Navigator>
   )
 }
