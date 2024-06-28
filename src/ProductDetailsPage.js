34ba7313f8a89232a66b65c283e13b5bb10a658f
SidhardhJoe <sidhardhjoe@gmail.com>
2024-06-28 18:26:54 +0530

    Page outline

--- src/ProductDetailsPage.js
+++ src/ProductDetailsPage.js
@@ -4,11 +4,11 @@ import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const ProductDetailsPage = ({route}) => {
+const ProductDetailsPage = ({ route }) => {
     const navigation = useNavigation();
     const [data, setData] = useState([]);
     const [val, setVal] = useState(0);
-    const {id} = route.params
+    const { id } = route.params
     const getAPI = async () => {
         console.log('id', id)
         try {
@@ -50,16 +50,37 @@ const ProductDetailsPage = ({route}) => {
                     </View>
                     <View  >
                         <View style={styles.capsule}>
-                            <TouchableOpacity onPress={() => setVal(val>0?val - 1:0)}>
+                            <TouchableOpacity onPress={() => setVal(val > 0 ? val - 1 : 0)}>
                                 <Image source={require("../Icons/Minus.png")} />
                             </TouchableOpacity>
-                            <Text>{val}</Text>
-                            <TouchableOpacity onPress={() => setVal(val<10?val + 1:10)}>
+                            <Text style={{ fontFamily: "PoppinsRegular" }}>{val}</Text>
+                            <TouchableOpacity onPress={() => setVal(val < 10 ? val + 1 : 10)}>
                                 <Image source={require("../Icons/Plus.png")} />
                             </TouchableOpacity>
                         </View>
                         <View>
                             <Text style={styles.stock}>{data?.stock}</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.sizeview}>
+                    <Text style={styles.sizetext}>Size</Text>
+                    <View style={styles.sizedetails}></View>
+                    <View>
+                        <Text style={styles.descriptiontext}>Description</Text>
+                        <Text>{data?.details}</Text>
+                    </View>
+                    <View style={styles.priceview}>
+                        <View>
+                            <Text>
+                                Total price
+                            </Text>
+                            <Text>
+                                {data?.price}
+                            </Text>
+                        </View>
+                        <View style={styles.pricebox}>
+
                         </View>
                     </View>
                 </View>
@@ -76,7 +97,7 @@ const styles = StyleSheet.create({
         backgroundColor: "black"
     },
     view1: {
-        height: "53%"
+        height: "50%"
     },
     image: {
         height: "100%",
@@ -84,7 +105,7 @@ const styles = StyleSheet.create({
         paddingTop: 10
     },
     view2: {
-        height: "47%",
+        height: "50%",
         backgroundColor: "white",
         borderTopLeftRadius: 30,
         borderTopRightRadius: 30,
@@ -114,7 +135,8 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
         marginHorizontal: 15,
         marginTop: 20,
-        gap: 20
+        gap: 20,
+        height: "28%",
 
     },
     stock: {
@@ -123,11 +145,35 @@ const styles = StyleSheet.create({
         color: "green"
 
     },
-    ratingContainer:{
-        flexDirection:"row"
+    ratingContainer: {
+        flexDirection: "row"
     },
-    reviewText:{
-        fontFamily:"PoppinsRegular",
-        marginLeft:2
+    reviewText: {
+        fontFamily: "PoppinsMedium",
+        marginLeft: 2
+    },
+    sizetext: {
+        fontFamily: "PoppinsBold",
+        fontSize: 20
+    },
+    sizeview: {
+        marginHorizontal: 15
+    },
+    sizedetails: {
+        backgroundColor: "black",
+        height: 50
+    },
+    descriptiontext: {
+        fontFamily: "PoppinsBold",
+        fontSize: 20
+    },
+    priceview:{
+        flexDirection:"row",
+         marginTop:"5%"
+    },
+    pricebox:{
+        height:"100%",
+        width:"45%",
+        backgroundColor:"black"
     }
 })
