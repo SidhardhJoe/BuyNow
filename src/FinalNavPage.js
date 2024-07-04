9b2661f4c25fdd6f463cd83dcf0b3700dceed4da
SidhardhJoe <sidhardhjoe@gmail.com>
2024-07-04 18:25:49 +0530

    1

--- src/FinalNavPage.js
+++ src/FinalNavPage.js
@@ -1,19 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
 
 const FinalNavPage = () => {
-  return (
-    <View style={styles.container}>
-      <Text>FinalNavPage</Text>
-    </View>
-  )
+    const navigation = useNavigation();
+    return (
+        <View style={styles.container}>
+            <View style={styles.view1}>
+                <TouchableOpacity onPress={() => navigation.navigate('PaymentPage')}>
+                    <Image source={require("../Icons/Back.png")} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.view2}>
+                <Text style={styles.headertxt}>Payment</Text>
+            </View>
+            <View>
+                
+            </View>
+        </View>
+    )
 }
 
 export default FinalNavPage
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:"white"
+    container: {
+        flex: 1,
+        backgroundColor: "white"
+    },
+    view1: {
+        marginTop: "12%",
+        paddingHorizontal: 20
+    },
+    view2:{
+        padding:20
+    },
+    headertxt:{
+        fontFamily:"PoppinsBold",
+        fontSize:20
     }
 })
