48f802d9f686d4b888f60aa79e21dc7ada293b53
SidhardhJoe <sidhardhjoe@gmail.com>
2024-07-08 13:07:37 +0530

    1

--- src/BottomSheet.js
+++ src/BottomSheet.js
@@ -1,20 +1,55 @@
-import { Button, Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, TouchableOpacity, View, Modal, Alert, Image } from 'react-native'
+import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { BlurView } from 'expo-blur';
 
 const BottomSheet = () => {
     const navigation = useNavigation();
+    const [modalVisible, setModalVisible] = useState(false);
     return (
         <View style={styles.container}>
             <Pressable onPress={() => navigation.goBack()} style={styles.topressable}>
             </Pressable>
-            <BlurView
-                experimentalBlurMethod='dimezisBlurView'
-                intensity={90}
-                tint="light"
-                style={styles.detailsview}>
-                <Button title='go back' onPress={() => navigation.goBack()} />
+            <BlurView experimentalBlurMethod='dimezisBlurView' intensity={99} tint="light" style={styles.detailsview}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Text style={styles.canceltext}>Cancel</Text>
+                </TouchableOpacity>
+                <View style={styles.view3}>
+                    <View style={styles.details}>
+                        <Text style={styles.innertext}>Subtotal:</Text>
+                        <Text style={styles.innertext1}>$500</Text>
+                    </View>
+                    <View style={styles.details}>
+                        <Text style={styles.innertext}>Shipping:</Text>
+                        <Text style={styles.innertext1}>$17.00</Text>
+                    </View>
+                    <View style={styles.details}>
+                        <Text style={styles.innertext}>Total:</Text>
+                        <Text style={styles.innertext1}>$517</Text>
+                    </View>
+                </View>
+                <TouchableOpacity style={styles.view4} onPress={() => setModalVisible(true)}>
+                    <View>
+                        <Text style={styles.pyotext}>Place your order</Text>
+                    </View>
+                </TouchableOpacity>
+                <Modal
+                    animationType="fade"
+                    transparent={true}
+                    visible={modalVisible}>
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Image source={require("../Icons/Baglast.png")} style={styles.baglogo} />
+                            <Text style={styles.successtxt}>Successfull!</Text>
+                            <View style={styles.viewsmall}>
+                            <Text style={styles.yohbsp}>Your order has been Successfully placed</Text>
+                            </View>
+                            <View style={styles.continuebox}>
+                                <Text style={styles.continuetext}>Continue Shopping</Text>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
             </BlurView>
         </View>
     )
@@ -30,10 +65,113 @@ const styles = StyleSheet.create({
         flex: 1
     },
     detailsview: {
-        height: "50%",
+        height: "30%",
         position: "absolute",
         bottom: 0,
-        width: "100%",
-        backgroundColor: "black"
+        width: "100%"
+    },
+    canceltext: {
+        fontFamily: "PoppinsRegular",
+        padding: 10,
+        fontSize: 16,
+        color: "red"
+    },
+    view3: {
+        height: "35%",
+        marginHorizontal: 20,
+        borderRadius: 10,
+        justifyContent: "center"
+    },
+    details: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginHorizontal: 10
+    },
+    innertext: {
+        fontFamily: "PoppinsBold",
+        fontSize: 16
+    },
+    innertext1: {
+        fontFamily: "PoppinsBold",
+        fontSize: 16
+    },
+    view4: {
+        height: "20%",
+        width: "60%",
+        backgroundColor: "black",
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        marginLeft: "20%",
+        marginTop: "8%"
+    },
+    pyotext: {
+        color: "white",
+        fontFamily: "PoppinsBold"
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalView: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+        width: "60%",
+        height: "30%"
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+    baglogo:{
+        height:25,
+        width:25,
+
+    },
+    continuebox:{
+        height:"20%",
+        width:"70%",
+        backgroundColor:"black",
+        borderRadius:10,
+        marginTop:"10%",
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    continuetext:{
+        color:"white",
+        fontFamily:"PoppinsBold",
+        fontSize:12
+    },
+    successtxt:{
+        fontFamily:"PoppinsBold",
+        marginTop:"5%"
+    },
+    viewsmall:{
+        width:"60%",
+        textAlign:"justify"
+    },
+    yohbsp:{
+        fontFamily:"PoppinsRegular",
+        fontSize:12
     }
 })
--- src/FinalNavPage.js
+++ src/FinalNavPage.js
@@ -5,22 +5,22 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const FinalNavPage = () => {
     const navigation = useNavigation();
-    const [data, setData]=useState();
+    const [data, setData] = useState();
 
-    const getData = async ()=>{
-        try{
+    const getData = async () => {
+        try {
             const response = await AsyncStorage.getItem("totalprice")
             const val = JSON.parse(response)
             console.log('val', val)
 
-        }catch(err){
+        } catch (err) {
             console.log('err', err)
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getData();
-    },[])
+    }, [])
 
 
 
@@ -37,7 +37,7 @@ const FinalNavPage = () => {
             <View style={styles.view3}>
                 <View style={styles.subview}>
                     <View style={styles.bggg}>
-                        <Image source={require("../Images/mastercard.png")} style={styles.logo}/>
+                        <Image source={require("../Images/mastercard.png")} style={styles.logo} />
                     </View>
                     <View>
                         <Text style={styles.insidetext}>MasterCard</Text>
@@ -45,7 +45,7 @@ const FinalNavPage = () => {
                 </View>
                 <View style={styles.subview}>
                     <View style={styles.bggg}>
-                        <Image source={require("../Images/PayPal.png")} style={styles.logo1}/>
+                        <Image source={require("../Images/PayPal.png")} style={styles.logo1} />
                     </View>
                     <View>
                         <Text style={styles.insidetext}>PayPal</Text>
@@ -53,7 +53,7 @@ const FinalNavPage = () => {
                 </View>
                 <View style={styles.subview}>
                     <View style={styles.bggg}>
-                        <Image source={require("../Images/Visa.png")} style={styles.logo2}/>
+                        <Image source={require("../Images/Visa.png")} style={styles.logo2} />
                     </View>
                     <View>
                         <Text style={styles.insidetext}>Visa Card</Text>
@@ -61,18 +61,22 @@ const FinalNavPage = () => {
                 </View>
                 <View style={styles.subview}>
                     <View style={styles.bggg}>
-                        <Image source={require("../Images/google.png")} style={styles.logo}/>
+                        <Image source={require("../Images/google.png")} style={styles.logo} />
                     </View>
                     <View>
                         <Text style={styles.insidetext}>Google Pay</Text>
                     </View>
                 </View>
                 <View style={styles.subview1}>
-                    <Image source={require("../Icons/Plus.png")} style={styles.logo5}/>
+                    <Image source={require("../Icons/Plus.png")} style={styles.logo5} />
                 </View>
             </View>
-            <View>
-                <Button title='press here ' onPress={()=> navigation.navigate('BottomSheet')}/>
+            <View style={styles.finalview}>
+                <TouchableOpacity style={styles.orderbox} onPress={()=>navigation.navigate("BottomSheet")}>
+                    <View >
+                        <Text style={styles.ordertext}>Confirm Order</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -89,62 +93,81 @@ const styles = StyleSheet.create({
         marginTop: "12%",
         paddingHorizontal: 20
     },
-    view2:{
-        padding:20
+    view2: {
+        padding: 20
     },
-    headertxt:{
-        fontFamily:"PoppinsBold",
-        fontSize:20
+    headertxt: {
+        fontFamily: "PoppinsBold",
+        fontSize: 20
     },
-    subview:{
-        height:"11%",
-        width:"90%",
-        flexDirection:"row",
-        borderWidth:1,
-        borderRadius:10,
-        alignItems:"center",
-        marginBottom:10
+    subview: {
+        height: "16%",
+        width: "90%",
+        flexDirection: "row",
+        borderWidth: 1,
+        borderRadius: 10,
+        alignItems: "center",
+        marginBottom: 10
     },
-    view3:{
-        alignItems:"center"
+    view3: {
+        alignItems: "center",
+        height: "40%"
     },
-    bggg:{
-        height:"70%",
-        width:"12%",
-        backgroundColor:"#F5F5F5",
-        borderRadius:50,
-        justifyContent:"center",
-        alignItems:"center",
-        marginHorizontal:10
+    bggg: {
+        height: "70%",
+        width: "12%",
+        backgroundColor: "#F5F5F5",
+        borderRadius: 50,
+        justifyContent: "center",
+        alignItems: "center",
+        marginHorizontal: 10
     },
-    logo:{
-        height:26,
-        width:26,
+    logo: {
+        height: 26,
+        width: 26,
     },
-    insidetext:{
-        fontFamily:"PoppinsMedium"
+    insidetext: {
+        fontFamily: "PoppinsMedium"
     },
-    logo1:{
-        height:24,
-        width:24,
+    logo1: {
+        height: 24,
+        width: 24,
     },
-    logo2:{
-        height:10,
-        width:25
+    logo2: {
+        height: 10,
+        width: 25
     },
-    subview1:{
-        height:"11%",
-        width:"90%",
-        flexDirection:"row",
-        borderWidth:1,
-        borderRadius:10,
-        alignItems:"center",
-        marginBottom:10,
-        borderColor:"whitesmoke",
-        justifyContent:"center"
+    subview1: {
+        height: "16%",
+        width: "90%",
+        flexDirection: "row",
+        borderWidth: 1,
+        borderRadius: 10,
+        alignItems: "center",
+        marginBottom: 10,
+        borderColor: "whitesmoke",
+        justifyContent: "center"
     },
-    logo5:{
-        height:28,
-        width:28
+    logo5: {
+        height: 28,
+        width: 28
+    },
+    finalview: {
+        height: "100%",
+        width: "100%",
+    },
+    orderbox: {
+        height: "8%",
+        width: "60%",
+        backgroundColor: "black",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+        marginLeft: "20%",
+        marginTop: "45%"
+    },
+    ordertext: {
+        color: "white",
+        fontFamily: "PoppinsBold"
     }
 })
